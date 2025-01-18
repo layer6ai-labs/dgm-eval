ae5c0ff624d61fd779467f272b2bc29946a46d88
Teddy298 <tadeuszdziarmaga8@gmail.com>
2025-01-18 20:08:31 +0100

    adding new metric energy

--- dgm_eval/__main__.py
+++ dgm_eval/__main__.py
@@ -229,6 +229,13 @@ def compute_scores(args, reps, test_reps, labels=None):
     scores = {}
     vendi_scores = None
 
+    if "energy" in args.metrics:
+        print("Computing Energy \n", file=sys.stderr)
+        scores["energy"] = compute_energy_with_reps_naive(*reps)
+        test_comparison = [reps[1], test_reps]
+        scores["energy_test"] = compute_energy_with_reps_naive(*test_comparison)
+        scores["MMM_energy"] = scores["energy_test"]/2 + (scores["energy_test"] / (scores["energy"] + scores["energy_test"])) / 2
+
     if "fd" in args.metrics:
         print("Computing FD \n", file=sys.stderr)
         scores["fd"] = compute_FD_with_reps(*reps)
